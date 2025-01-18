6c263264ece75b17c6d297dd5a3f3061f35796d3
Felipetecnico <juniofelipe134@gmail.com>
2025-01-17 21:52:18 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="style.css">config.php
+<?php
+$host = 'localhost';
+$db = 'usuarios';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro: " . $e->getMessage());
+}
+?>
---
+++ listar.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="style.css">
+<?php
+include 'config.php';
+
+$sql = "SELECT * FROM users";
+$stmt = $pdo->query($sql);
+
+while ($user = $stmt->fetch()) {
+    echo "Nome: " . $user['nome'] . " - E-mail: " . $user['email'] . "<br>";
+}
+?>
